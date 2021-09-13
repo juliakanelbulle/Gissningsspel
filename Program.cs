8bbaf683fc3b45c447f1e4350b35d13e063fa160
juliakanelbulle <90604362+juliakanelbulle@users.noreply.github.com>
2021-09-13 10:11:04 +0200

    skapat projekt

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pancake1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
